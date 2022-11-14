1c4d2bbb69c6c914c08889ac838cd59c9dc31a34
alonsodm12 <alonsodmx@gmail.com>
2022-11-14 17:55:26 +0100

    p

---
+++ ej03_tdas_letras/estudiante/src/bolsa_letras.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include "letters_bag.h"
+#include "letters_set.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    LetterSet conjunto;
+
+
+
+    return 0;
+}
---
+++ ej03_tdas_letras/estudiante/src/conjunto_letras.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include "letters_set.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+    
+
+
+
+
+
+
+
+}
